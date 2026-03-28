9e8926aa811c7a76033a184e8c70a7155fe5089f
Luz Moreno Campos <luzdirection@gmail.com>
2026-03-28 15:32:32 -0500

    created event listener to toggle log in modal

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const loginBtn = document.getElementById("login-menu-btn");
+const loginModal = document.getElementById("login-modal");
+
+loginBtn.addEventListener("click", () => {
+    loginModal.classList.toggle("hidden");
+});
