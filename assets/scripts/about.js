64c2d138eb287e5fedeb0145f46702a20c287390
jaspreet <jasspreet4640@gmail.com>
2026-03-31 15:04:53 -0500

    fix

---
+++ assets/scripts/about.js
@@ -0,0 +1,30 @@
+'use strict';
+document.addEventListener("DOMContentLoaded", function () {
+    let videos = document.querySelectorAll(".video-slide");
+    let prevBtn = document.querySelector(".prev-btn");
+    let nextBtn = document.querySelector(".next-btn");
+    let index = 0;
+
+    function showVideo(i) {
+        videos.forEach(function(video) {
+            video.classList.remove("active");
+            video.pause();
+            video.currentTime = 0;
+        });
+
+        videos[i].classList.add("active");
+        videos[i].play();
+    }
+
+    if (nextBtn && prevBtn && videos.length > 0) {
+        nextBtn.addEventListener("click", function () {
+            index = (index + 1) % videos.length;
+            showVideo(index);
+        });
+
+        prevBtn.addEventListener("click", function () {
+            index = (index - 1 + videos.length) % videos.length;
+            showVideo(index);
+        });
+    }
+});
